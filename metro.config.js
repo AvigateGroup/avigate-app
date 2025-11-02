b6e365202ab7239d06a545576ee67ac628fb1be2
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-11-02 09:05:01 +0100

    metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = {
+  ...config,
+  server: {
+    enhanceMiddleware: (middleware) => {
+      return (req, res, next) => {
+        // Force the server to use LAN IP
+        return middleware(req, res, next);
+      };
+    },
+  },
+};
