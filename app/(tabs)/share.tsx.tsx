8399686d048b39a33a35afc3e9c58a6a64d28c62
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-11-04 13:05:48 +0100

    Share Location tab works

---
+++ app/(tabs)/share.tsx.tsx
@@ -0,0 +1,9 @@
+// app/(tabs)/share.tsx
+// Share Location screen
+
+import React from 'react';
+import { ShareLocationScreen } from '../../src/screens/share/ShareLocationScreen';
+
+export default function Share() {
+  return <ShareLocationScreen />;
+}
