88862f04945a00916385f7d3458735c5cca023e7
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-11-04 13:05:37 +0100

    Action buttons properly positioned

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,9 @@
+// app/search.tsx
+// Search Destination screen (modal)
+
+import React from 'react';
+import { SearchDestinationScreen } from '../../src/screens/search/SearchDestinationScreen';
+
+export default function Search() {
+  return <SearchDestinationScreen />;
+}
