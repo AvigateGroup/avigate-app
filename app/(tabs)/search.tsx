85d32ef5fcd89d896f4ebdf9fe48f55d5e23735e
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-11-05 20:14:17 +0100

    delete unused files

--- app/(tabs)/search.tsx
+++
@@ -1,9 +0,0 @@
-// app/search.tsx
-// Search Destination screen (modal)
-
-import React from 'react';
-import { SearchDestinationScreen } from '../../src/screens/search/SearchDestinationScreen';
-
-export default function Search() {
-  return <SearchDestinationScreen />;
-}
