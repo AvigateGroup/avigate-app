8172054b6b889a50b0c32178fb7ff0f4d84d3a56
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-11-06 05:14:07 +0100

    feat: device management

---
+++ app/profile/devices.tsx
@@ -0,0 +1,8 @@
+// app/profile/devices.tsx
+// Device management screen - View and manage logged-in devices
+
+import { DevicesScreen } from '../../src/screens/profile/DevicesScreen';
+
+export default function Devices() {
+  return <DevicesScreen />;
+}
