21cd37280abf928eba586761cbed57fcb319972b
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2026-02-02 08:38:34 +0100

    Add module-resolver babel plugin for @ path aliases

--- avigate-app/babel.config.js
+++ avigate-app/babel.config.js
@@ -3,7 +3,24 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin', 
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@': './src',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@api': './src/api',
+            '@utils': './src/utils',
+            '@constants': './src/constants',
+            '@types': './src/types',
+            '@store': './src/store',
+            '@navigation': './src/navigation',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
     ],
   };
 };
